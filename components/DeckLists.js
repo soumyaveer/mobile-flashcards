ed87b47008e6725516dce3c337b8365c8ec43bdc
Soumya Veer <veer.soumya@gmail.com>
2019-12-02 19:21:17 -0800

    Add DeckList Component

---
+++ components/DeckLists.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class DeckLists extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>DeckList Component inserted!!</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckLists;
