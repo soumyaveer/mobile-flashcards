c6943ed43c37dde92accc0446d3e60aae431eb9f
Soumya Veer <veer.soumya@gmail.com>
2019-12-02 20:04:31 -0800

    Display Deck Lists

--- App.js
+++ App.js
@@ -3,9 +3,26 @@ import DeckLists from './components/DeckLists';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  state = {
+    decks: [
+      {
+        name: "Deck 1",
+        numberOfCards: 3
+      },
+      {
+        name: "Deck 2",
+        numberOfCards: 2
+      },
+      {
+        name: "Deck 3",
+        numberOfCards: 4
+      }
+    ]
+  };
+
   return (
     <View style={styles.container}>
-      <DeckLists/>
+      <DeckLists decks={state.decks}/>
     </View>
   );
 }
--- components/DeckLists.js
+++ components/DeckLists.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-class DeckLists extends React.Component {
+class DeckLists extends Component {
+
+  buildData = () => {
+    const { decks } = this.props
+    return decks.map(deck => {
+      return {
+        key: deck.name,
+        name: deck.name,
+        numberOfCards: deck.numberOfCards
+      }
+    })
+  };
+
   render() {
+    console.log(this.props.decks)
+    const data = this.buildData();
+
     return (
-      <View>
-        <Text>DeckList Component inserted!!</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <Text style={styles.name}>{item.name} - {item.numberOfCards}</Text>
+          )}
+        />
       </View>
     )
   }
 }
 
 export default DeckLists;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
